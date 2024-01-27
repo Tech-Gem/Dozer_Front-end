c63911cd4fd0ede863550fd03a35899c55c61acc
Hiwot Derese <Hiwotderese16@gmail.com>
2024-01-27 12:37:33 +0300

    implement welcome page

---
+++ Dozer_Mobile/dozer_owner/lib/features/welcome.dart
@@ -0,0 +1,88 @@
+import 'package:DozerOwner/core/utils/colors.dart';
+import 'package:DozerOwner/features/authentication/presentation/pages/login_page.dart';
+import 'package:DozerOwner/features/authentication/presentation/pages/signup_page.dart';
+import 'package:DozerOwner/features/widgets/rounded_button.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Align(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/images/logo.jpg',
+                width: 60.w,
+                height: 45.h, // Responsive width based on screen width
+              ),
+              Text(
+                'Welcome to Dozer',
+                style: TextStyle(
+                  fontSize: 20.sp, // Responsive font size based on screen width
+                  fontWeight: FontWeight.bold,
+                  color: greyTextColor,
+                ),
+              ),
+              SizedBox(
+                height: 10.h,
+              ), // Responsive height
+              RoundedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SignupPage(),
+                    ),
+                  );
+                  // Handle Signup button press
+                },
+                height: 6.h, // Responsive height
+                width: 80.w, // Responsive width
+                buttonColor: primaryColor,
+                child: Text(
+                  "Signup",
+                  style: TextStyle(
+                    fontSize:
+                        18.sp, // Responsive font size based on screen width
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ), // Custom button color
+              ),
+              SizedBox(height: 4.h), // Responsive height
+
+              RoundedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LoginPage(),
+                    ),
+                  );
+                  // Handle Login button press
+                },
+                height: 6.h, // Responsive height
+                width: 80.w, // Responsive width
+                buttonColor: primaryColor,
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                    fontSize:
+                        18.sp, // Responsive font size based on screen width
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ), // Custom button color
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
